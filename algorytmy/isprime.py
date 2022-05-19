75ec0a21d98f34ce5735adde26060664434dc6c2
Jerzy Szyjut <46868774+jerzyszyjut@users.noreply.github.com>
2022-05-19 19:29:41 +0200

    Update isprime.py

--- algorytmy/isprime.py
+++ algorytmy/isprime.py
@@ -2,8 +2,10 @@ num = 11
 
 
 def is_prime(num):
-    if num < 3:
+    if num < 2:
         return False
+    if num == 2:
+        return True
     for i in range(3, int(num**(1/2))+1, 2):
         if (num % i) == 0:
             return False
